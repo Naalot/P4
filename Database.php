4b93b11973cdafb8b47cbb9c3bb9c212b08d2b99
Naalot <laurent_bureau@gmx.fr>
2020-08-19 11:21:09 +0200

    WZW Chap.3

---
+++ Database.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Conexion à la base de données
+ */
+class Database
+{
+    //Nos constantes
+    const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+ 
+    /**
+     * Méthode de connexion à notre base de données 
+     *
+     * @return void
+     */
+    public function getConnection()
+    {
+        //Tentative de connexion à la base de données
+        try {
+            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //On renvoie un message avec le mot-clé return
+            return 'Connexion OK';
+        }
+        //On lève une erreur si la connexion échoue
+        catch (Exception $errorConnection) {
+            die('Erreur de connection :' . $errorConnection->getMessage());
+        }
+    }
+}
---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+//On inclut le fichier dont on a besoin (ici à la racine de notre site)
+require 'Database.php';
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- -->
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+  <title>Mon blog</title>
+</head>
+
+<body>
+  <div>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+
+    <?php
+
+    $db = new Database();
+    //On ajoute un echo pour vérifier qu'un message s'affiche à l'écran
+    echo $db->getConnection();
+    ?>
+
+  </div>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
