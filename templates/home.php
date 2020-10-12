9bd31fcc49049c59dce1765d82dec411f2fb0cf7
Naalot <laurent_bureau@gmx.fr>
2020-10-12 09:16:53 +0200

    WzW Chap. View

--- templates/home.php
+++ templates/home.php
@@ -1,54 +1,17 @@
-<!doctype html>
-<html lang="fr">
+<?php $this->title = "Accueil"; ?>
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- -->
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
-
-  <title>Mon blog</title>
-</head>
-
-<body>
-
+<h1>Mon blog</h1>
+<p>En construction</p>
+<?php
+foreach ($articles as $article) {
+?>
   <div>
-
-    <h1>Mon blog</h1>
-    <p>En construction</p>
-
-    <?php
-    foreach ($articles as $article) {
-    ?>
-
-      <div>
-        <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId()); ?>"><?= htmlspecialchars($article->getTitle()); ?></a></h2>
-        <p><?= htmlspecialchars($article->getContent()); ?></p>
-        <p><?= htmlspecialchars($article->getAuthor()); ?></p>
-        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
-      </div>
-      <br>
-      
-    <?php
-    }
-    ?>
-
+    <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId()); ?>"><?= htmlspecialchars($article->getTitle()); ?></a></h2>
+    <p><?= htmlspecialchars($article->getContent()); ?></p>
+    <p><?= htmlspecialchars($article->getAuthor()); ?></p>
+    <p>Créé le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
   </div>
-
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-  </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+  <br>
+<?php
+}
+?>
