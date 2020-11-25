02a05cca9373da9b132201d86be7290d9bf62d29
Naalot <laurent_bureau@gmx.fr>
2020-11-25 10:24:17 +0100

    29. Espace d'administration

--- templates/home.php
+++ templates/home.php
@@ -14,29 +14,31 @@
 <?= $this->session->show('delete_account'); ?>
 <?php
 if ($this->session->get('pseudo')) {
-    ?>
+?>
     <a href="../public/index.php?route=logout">Déconnexion</a>
     <a href="../public/index.php?route=profile">Profil</a>
+    <?php if ($this->session->get('role') === 'admin') { ?>
+        <a href="../public/index.php?route=administration">Administration</a>
+    <?php } ?>
     <a href="../public/index.php?route=addArticle">Nouvel article</a>
-    <?php
+<?php
 } else {
-    ?>
+?>
     <a href="../public/index.php?route=register">Inscription</a>
     <a href="../public/index.php?route=login">Connexion</a>
-    <?php
+<?php
 }
 ?>
 <?php
-foreach ($articles as $article)
-{
-    ?>
+foreach ($articles as $article) {
+?>
     <div>
-        <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h2>
-        <p><?= htmlspecialchars($article->getContent());?></p>
-        <p><?= htmlspecialchars($article->getAuthor());?></p>
-        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
+        <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId()); ?>"><?= htmlspecialchars($article->getTitle()); ?></a></h2>
+        <p><?= htmlspecialchars($article->getContent()); ?></p>
+        <p><?= htmlspecialchars($article->getAuthor()); ?></p>
+        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
     </div>
     <br>
-    <?php
+<?php
 }
 ?>
