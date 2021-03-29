ff5852d7ae3b30dcabcc090480e1af2b20e9ff50
Naalot <laurent_bureau@gmx.fr>
2021-03-29 12:30:28 +0200

    Error 404 / Error 500

--- templates/error_404.php
+++ templates/error_404.php
@@ -1,2 +1,8 @@
-
 <!-- 'page non trouvée'-->
+<h1 class="center">
+    Error 404 - Not found
+</h1>
+
+<p class="center mt-2">
+    Le fichier requis n'a pas été trouvé. Il peut s'agir d'une erreur technique. Veuillez réessayer ultérieurement. Si vous ne pouvez pas accéder au fichier après plusieurs tentatives, cela signifie qu'il a été supprimé.
+</p>
--- templates/error_500.php
+++ templates/error_500.php
@@ -1,2 +1,9 @@
-
 <!--  'problème serveur' -->
+<h1 class="center">
+    Error 500 - Internal Server Error
+</h1>
+
+<p class="center mt-2">
+    Le Serveur a rencontré une erreur interne ou un problème de configuration et n'a pas pu accéder à votre requête. <br>
+    Merci de contacter l'administrateur du serveur à cette adresse : <a href="mailto:laurent_bureau@gmx.fr">laurent_bureau@gmx.fr</a>
+</p>
